bc531854f443f515480a2e09c1a27d658fcbcaaa
Jacob Heider <jacob@tea.xyz>
2023-07-12 21:33:10 -0400

    provide `tea-cli` bottles
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    
    wip
    
    wip
    
    wip (better)
    
    
    wip
    
    
    needs more perms for GITHUB_OUTPUT
    
    
    wip
    
    
    wpi
    
    
    ChatGPT lied to me
    
    
    hopefully done
    
    
    done

--- .github/actions/bump.ts
+++ .github/actions/bump.ts
@@ -7,10 +7,9 @@ args:
   - --allow-net
   - --allow-run
   - --allow-read
-  - --allow-write=.
+  - --allow-write
   - --allow-env
   - --unstable
-  - --import-map=https://raw.githubusercontent.com/teaxyz/cli/v0.21/import-map.json
 ---*/
 
 import { backticks, run, panic } from "utils"
@@ -18,8 +17,10 @@ import { basename } from "deno/path/mod.ts"
 import { crypto, toHashString } from "deno/crypto/mod.ts"
 
 const tap = Deno.env.get("TAP") ?? panic("No TAP specified")
+const rootUrl = "https://github.com/teaxyz/homebrew-pkgs/releases/download"
 
 const livecheck: LCResults[] = JSON.parse(await backticks({ cmd: ["brew", "livecheck", "--quiet", "--newer-only", "--full-name", "--json", `--tap=${tap}`] }))
+// const livecheck: LCResults[] = JSON.parse(await backticks({ cmd: ["brew", "livecheck", "--quiet", "--newer-only", "--full-name", "--json", `./tea-cli.rb`] }))
 
 if (livecheck.length === 0) {
   console.log("No outdated packages")
@@ -42,13 +43,23 @@ for (const pkg of livecheck) {
     const url = formula.match(/  url "(.*)"/)?.[1]
     const newUrl = url.replaceAll(oldVersion, newVersion)
     const newSha = await sha256(newUrl)
+
+    const bottles = await bottle(newVersion)
+
+    // Generate new bottle block.
+    const newBottleBlock = generateBottleBlock(bottles, newVersion)
+
     const newFormula = formula.replaceAll(url, newUrl).replace(/  sha256 ".+"/, `  sha256 "${newSha}"`)
+                              .replace(/bottle do[\s\S]*?end/m, newBottleBlock)
 
     await Deno.writeTextFile(`${name}.rb`, newFormula)
 
     if (Deno.env.get("GITHUB_ACTIONS")) {
       await run({ cmd: ["git", "add", `${name}.rb`] })
       await run({ cmd: ["git", "commit", "-m", `bump ${name} from ${oldVersion} to ${newVersion}`] })
+
+      const envFile = Deno.env.get("GITHUB_OUTPUT")!
+      await Deno.writeTextFile(envFile, `name=${name}\nversion=${newVersion}\n`, { append: true})
     }
 
     console.log(`Bumped ${name} from ${oldVersion} to ${newVersion}`)
@@ -69,4 +80,89 @@ async function sha256(url: string) {
   const file = await fetch(url).then(res => res.arrayBuffer())
   const hash = await crypto.subtle.digest("SHA-256", file)
   return toHashString(hash)
+}
+
+interface Bottle {
+  platform: string
+  sha256: string
+  url: string
+}
+
+async function bottle(newVersion: string): Promise<Bottle[]> {
+  await run({ cmd: ["mkdir", "-p", "bottles"] })
+  const platforms = ["darwin+aarch64", "darwin+x86-64", "linux+x86-64"] // "linux+aarch64" not supported for bottles yet
+
+  const bottles: Bottle[] = []
+
+  for (const platform of platforms) {
+    const bottleUrl = `https://github.com/teaxyz/cli/releases/download/v${newVersion}/tea-${newVersion}+${platform}.tar.xz`
+    const prefix = platform === "darwin+aarch64" ? "opt/homebrew" : "usr/local/Homebrew"
+
+    // Download the tarball
+    const response = await fetch(bottleUrl)
+    const file = await response.arrayBuffer()
+
+    // Convert ArrayBuffer to Uint8Array for Deno.writeFile
+    const uint8Array = new Uint8Array(file)
+    const binaryFileName = `tea-${newVersion}+${platform}.tar.xz`
+
+    // Save the binary to a file
+    await Deno.writeFile(binaryFileName, uint8Array)
+
+        // Build the directory structure
+    const cellarPath = `tea-cli/${newVersion}`
+    await run({ cmd: ["mkdir", "-p", `${cellarPath}/bin`] })
+
+    // Unpack the binary
+    await run({ cmd: ["tar", "-Jxf", binaryFileName, "-C", `${cellarPath}/bin`] })
+
+    // Create INSTALL_RECEIPT.json
+    const receipt = {
+      formula: "TeaCli",
+      version: newVersion,
+      built_as_bottle: true,
+      plist: null,
+      time: new Date().getTime(),
+      source_files: `/${prefix}/Library/Taps/teaxyz/cli/Formula/TeaCli.rb`,
+      source_modified_time: new Date().getTime(),
+      HEAD: "HEAD",
+      stdlib: null,
+      arch: platform.split("+")[1],
+    }
+    await Deno.writeTextFile(`${cellarPath}/INSTALL_RECEIPT.json`, JSON.stringify(receipt))
+
+    // Compress everything into a .tar.gz
+    const platformTag = mapPlatformToBottleTag(platform)
+    const bottleFilename = `bottles/tea-cli-${newVersion}.${platformTag}.bottle.tar.gz`
+
+    await run({ cmd: ["tar", "-czf", bottleFilename, cellarPath] })
+    await run({ cmd: ["rm", "-rf", cellarPath] })
+
+    const sha256Bottle = (await backticks({cmd: ["shasum", "-a", "256", bottleFilename]})).split(" ")[0]
+    const uploadUrl = `${rootUrl}/v${newVersion}/${bottleFilename}`
+    bottles.push({ platform: platformTag, sha256: sha256Bottle, url: uploadUrl })
+  }
+  return bottles
+}
+
+function mapPlatformToBottleTag(platform: string): string {
+  // Map your platform strings to Homebrew bottle tags
+  // This is just an example; adapt it to your needs
+  switch (platform) {
+    case 'darwin+aarch64': return 'arm64_big_sur'
+    case 'darwin+x86-64': return 'big_sur'
+    // case 'linux+aarch64': return 'arm64_linux'  // not supported for bottles yet
+    case 'linux+x86-64': return 'x86_64_linux'
+    default: throw new Error(`Unknown platform: ${platform}`)
+  }
+}
+
+function generateBottleBlock(bottles: Bottle[], version: string) {
+  let bottleBlock = "bottle do\n"
+  for (const { platform, sha256 } of bottles) {
+    bottleBlock += `    sha256 cellar: :any_skip_relocation, ${platform}: "${sha256}"\n`
+  }
+  bottleBlock += `    root_url "${rootUrl}/v${version}"\n`
+  bottleBlock += "  end\n"
+  return bottleBlock
 }
